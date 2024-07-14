0f8bee9441126c5ce74ff1ceed7b9afa82fc9b3a
Bharathi T <bharathitulasi3@gmail.com>
2024-07-14 20:20:13 +0530

    Add model and  app

---
+++ app/main.py
@@ -0,0 +1,124 @@
+
+import pickle
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def get_cleaned_data():
+  data = pd.read_csv("data/Housing.csv")
+  
+  data = pd.get_dummies(data = data)
+  return data
+
+
+def col1_input(input_slider_dict1, input_slider_dict2):
+  
+  st.subheader("Select your options")
+  data = get_cleaned_data()
+
+  slider_labels1 = [("Area", "area"),
+                   ("Number of Bathrooms", "bathrooms"),
+                   ("Number of stories", "stories"), 
+                   ]
+  slider_labels2 = [("Parking slots", "parking")]
+    
+    
+
+  for label, key in slider_labels1:
+    input_slider_dict1[key] = st.number_input(
+        label,
+        min_value = 1,
+        max_value = data[key].max()
+      )
+  
+  for label, key in slider_labels2:
+    input_slider_dict2[key] = st.number_input(
+        label,
+        min_value = 0,
+        max_value = data[key].max()
+      )
+  
+  input_dict1 = {**input_slider_dict1, **input_slider_dict2}
+  return input_dict1
+
+
+def col2_input(input_dict2):
+  st.subheader("Select your options")
+  data = get_cleaned_data()
+
+  radio_labels = [("Mainroad", "mainroad_yes"), 
+                    ("Guestroom", "guestroom_yes"), 
+                    ("Basement", "basement_yes"), 
+                    ("Hot water Heating", "hotwaterheating_yes"), 
+                    ("Airconditiong", "airconditioning_yes"),
+                    ("Preferred Area", "prefarea_yes"),
+                    ("Furnished", "furnishingstatus_furnished")
+                  ]
+
+    
+
+  for label, key in radio_labels:
+    input_dict2[key] = st.checkbox(label)
+    input_dict2 = {keys: int(value) for keys, value in input_dict2.items()}
+
+  return input_dict2  
+
+
+def do_predictions(input_data):
+  model = pickle.load(open("model/model.pkl", "rb"))
+  scaler = pickle.load(open("model/scaler.pkl", "rb"))
+
+  input_arr = np.array(list(input_data.values())).reshape(1,-1)
+  input_arr_scaled = scaler.transform(input_arr)
+
+  prediction = model.predict(input_arr_scaled)
+  
+  st.header(int(prediction[0]))
+
+
+
+def main():
+  st.set_page_config(
+  page_title = "House Price Predictor",
+  page_icon = ":house_with_garden:",
+  layout = "wide",
+  initial_sidebar_state = "expanded"
+
+  ) 
+
+  with st.container():
+    st.title("House Price Predictor")
+    st.header("Know the approximate price of your dream house!")
+   
+   
+  col1, col2, col3  = st.columns([1, 1, 1])
+   
+  
+  input_slider_dict1 = {}
+  input_slider_dict2 = {}
+  input_dict1 = {}
+  input_dict2 = {}
+
+  with col1:
+    input1 = col1_input(input_slider_dict1, input_slider_dict2)
+      
+  
+
+  with col2:
+    input2 = col2_input(input_dict2)
+
+    
+
+
+  input_data = {**input1, **input2}
+  
+  
+
+  with col3:
+    if st.button("**Predict My Dream House Price**", type = "primary", use_container_width = True):
+      do_predictions(input_data) 
+    
+
+    
+if __name__ == "__main__":
+    main()
---
+++ model/main.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import pickle
+
+def get_cleaned_data():
+
+  #read the data file
+  data = pd.read_csv("data/Housing.csv")
+  
+  # Get dummy values for categorical features
+  data = pd.get_dummies(data = data)
+
+  
+
+  return data
+
+def create_model(data):
+  # input and output
+  X = data.drop(["price", "bedrooms","mainroad_no", "guestroom_no", "basement_no", "airconditioning_no", "prefarea_no", "hotwaterheating_no", "furnishingstatus_semi-furnished", "furnishingstatus_unfurnished"], axis = 1)
+  y = data["price"]
+  
+
+  # Variable Inflation Factor (VIF) to check multicollinearity and to drop unnecessary features
+  # 'X' is the DataFrame of independent variables
+  vif = pd.DataFrame()
+  vif["Variable"] = X.columns
+  vif["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+
+  print("VIF", vif)
+  print(X.head)
+
+  #scaling
+  scaler = StandardScaler()
+  X_scaled = scaler.fit_transform(X.values)
+
+  # split the data
+  x_train, x_test, y_train, y_test = train_test_split(X_scaled, y, test_size = 0.2, random_state = 42)
+
+  # train the model
+  model = LinearRegression()
+  model.fit(x_train, y_train)
+  r2 = model.score(x_train, y_train)
+  n = x_train.shape[0]
+  p = x_train.shape[1]
+  adj_r2 = 1 - (1-r2)*(n-1)/(n-p-1)
+  print("r-score: ", r2)
+  print("adjusted_r2: ", adj_r2)
+
+  
+  # test the model
+  y_pred = model.predict(x_test)
+  
+  
+  return model, scaler
+
+
+
+
+
+def main():
+  data = get_cleaned_data()
+  model, scaler = create_model(data)
+  
+  with open("model/model.pkl", "wb") as f:
+    pickle.dump(model, f)
+  with open("model/scaler.pkl", "wb") as f:
+    pickle.dump(scaler, f)
+
+
+if __name__ == '__main__':
+  main()
